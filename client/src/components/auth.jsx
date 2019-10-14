2dce01c8a3bc0d96a91140b5dcac1db0c62006c9
antontrofymchuk <antontrofymchuk@gmail.com>
2019-10-14 21:57:25 +0300

    reverted auth.jsx

--- client/src/components/auth.jsx
+++ client/src/components/auth.jsx
@@ -4,74 +4,55 @@ import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { post } from '../utils/httpService';
 import { LOGIN_URL } from '../utils/constants';
 
-export class Auth extends React.Component {
-  constructor(props) {
-    super(props);
+export const Auth = (props) => {
+  const [login, setLogin] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-    this.state = {
-      login: '',
-      password: ''
-    };
-
-    this.loginUser = this.loginUser.bind(this);
-    this.setLogin = this.setLogin.bind(this);
-    this.setPassword = this.setPassword.bind(this);
-  }
-
-  setLogin(e) {
-    this.setState({ login: e.target.value });
-  }
-
-  setPassword(e) {
-    this.setState({ password: e.target.value });
-  }
-
-  loginUser() {
+  const loginUser = () => {
     post(LOGIN_URL, {
-      login: this.state.login,
-      password: this.state.password
+      login: login,
+      password: password
     }).then(({ data }) => {
       alert(`Logged: ${data.success}`);
       if (data.success) {
-        this.props.onHide();
-        this.props.setUser(this.state.login);
-        sessionStorage.setItem('user', this.state.login);
+        props.onHide();
+        props.setUser(login);
+        sessionStorage.setItem('user', login);
       }
     });
   }
 
-  render() {
-    return (
-      <Container>
-        <Row className='justify-content-center'>
-          <Col xs={6}>
-            <Form>
-              <Form.Group controlId='formUsername'>
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type='input'
-                  placeholder='Enter username'
-                  value={this.login}
-                  onChange={this.setLogin}
-                />
-              </Form.Group>
+  return (
+    <Container>
+      <Row className='justify-content-center'>
+        <Col xs={6}>
+          <Form>
+            <Form.Group controlId='formUsername'>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type='input'
+                placeholder='Enter username'
+                value={login}
+                onChange={(e) => setLogin(e.target.value)}
+              />
+            </Form.Group>
 
-              <Form.Group controlId='formBasicPassword'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type='password'
-                  placeholder='Password'
-                  value={this.password}
-                  onChange={this.setPassword}
-                />
-              </Form.Group>
-              <Button variant='primary' onClick={this.loginUser}>
-                Login
+            <Form.Group controlId='formBasicPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant='primary' onClick={loginUser}>
+              Login
               </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+
 }
