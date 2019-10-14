94565b542aa863ad373135b62e7c0b3fffb67a32
Anna Maier <anna_maier@epam.com>
2019-10-14 15:56:08 +0300

    add react-router and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import { Login } from './components/login';
+import { CarouselView } from './components/carousel';
 
 function App() {
-  return <div className="App">
-    <Login />
-  </div>;
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Sign in</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <CarouselView />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
