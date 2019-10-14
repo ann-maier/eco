957af1dc01a73aeb1933fe2e33691ec05e3f610d
antontrofymchuk <antontrofymchuk@gmail.com>
2019-10-14 10:03:31 +0300

    changed port

--- backend/server/app.js
+++ backend/server/app.js
@@ -13,6 +13,6 @@ app.use(corsMiddleware);
 
 app.use('/', router);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
