c840cc29e9bb04c2aae326a00734d3b5e5d74532
Anton Trofymchuk <antontrofymchuk@gmail.com>
2019-10-14 13:05:50 +0300

    Styling/login form (#3)
    * work with login
    
    * work with login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { Auth } from './components/auth';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Login } from './components/login';
 
-const App: React.FunctionComponent = () => (
-  <div className="App">
-    <Auth />
-  </div>
-);
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  )
+};
 
 export default App;
--- client/src/components/auth.tsx
+++ client/src/components/auth.tsx
@@ -2,32 +2,51 @@ import React from 'react';
 
 import { post } from '../utils/httpService';
 import { LOGIN_URL } from '../utils/constants';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
-export const Auth: React.FunctionComponent = () => {
+export const Auth = (props: any) => {
   const [login, setLogin] = React.useState('');
   const [password, setPassword] = React.useState('');
 
   const loginUser = () => {
     post(LOGIN_URL, { login, password }).then(({ data }) => {
       alert(`Logged: ${data.success}`);
+      if (!!data.success) {
+        props.onHide();
+      }
     });
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="login"
-        value={login}
-        onChange={e => setLogin(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <button onClick={loginUser}>Login</button>
-    </div>
+    <Container>
+      <Row className="justify-content-center">
+        <Col xs={6}>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="input"
+                placeholder="Enter username"
+                value={login}
+                onChange={(e: any) => setLogin(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={loginUser}>
+              Login
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
