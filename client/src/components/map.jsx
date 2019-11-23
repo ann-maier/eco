a6b084adb3e82bfe47e913ba0c9811873704f676
antontrofymchuk <antontrofymchuk@gmail.com>
2019-11-23 18:52:12 +0200

    polygons functionality

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect } from 'react'
+import { Map as LeafletMap, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
+
+import { get } from '../utils/httpService';
+import { POLYGONS_URL } from '../utils/constants';
 
 export const Map = () => {
+
+  useEffect(() => {
+    get(POLYGONS_URL);
+  }, []);
+
   return (
     <LeafletMap
       center={[50.4547, 30.5238]}
@@ -18,6 +26,11 @@ export const Map = () => {
       <TileLayer
         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
       />
+      <Polygon positions={[
+        [48.2539411446343, 33.310546875],
+        [48.5747899109288, 34.365234375],
+        [48.0633965377621, 34.12353515625]]
+      } color="blue" onClick={(e) => console.log('he')} />
       <Marker position={[50.4547, 30.5238]}>
         <Popup>
           Popup for any custom information.
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,1 +1,2 @@
 export const LOGIN_URL = 'http://localhost:8080/login';
+export const POLYGONS_URL = 'http://localhost:8080/polygons';
