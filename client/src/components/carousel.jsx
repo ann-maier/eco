114264e457234a7ce441eb98b4841622c5f88672
Anna Maier <anna_maier@epam.com>
2019-11-23 14:35:13 +0200

    replace google maps to be leaflet maps

--- client/src/components/carousel.jsx
+++ client/src/components/carousel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel, Col, Container, Row } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
 import img1 from '../images/1.jpg';
 import img2 from '../images/2.jpg';
--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import { Chart } from 'react-google-charts';
+import React from 'react'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
-import { MAP_API_KEY } from '../utils/constants';
-
-export const Map = () => (
-  <Chart
-    width="100%"
-    height="100%"
-    chartType="GeoChart"
-    data={[
-      ['Country'],
-      ['Ukraine'],
-    ]}
-    options={{
-      region: 'UA',
-    }}
-    mapsApiKey={MAP_API_KEY}
-  />
-)
+export const Map = () => {
+  return (
+    <LeafletMap
+      center={[50.4547, 30.5238]}
+      zoom={10}
+      maxZoom={15}
+      attributionControl={true}
+      zoomControl={true}
+      doubleClickZoom={true}
+      scrollWheelZoom={true}
+      dragging={true}
+      animate={true}
+      easeLinearity={0.35}
+    >
+      <TileLayer
+        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={[50.4547, 30.5238]}>
+        <Popup>
+          Popup for any custom information.
+          </Popup>
+      </Marker>
+    </LeafletMap>
+  );
+}
