485622db31c28f062530cde7ecec6690eacb6a29
antontrofymchuk <antontrofymchuk@gmail.com>
2019-12-22 20:26:13 +0200

    open popup after clicking on the marker

--- client/src/components/points.jsx
+++ client/src/components/points.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Circle, Popup, Marker, Leaf } from 'react-leaflet';
+import { Popup, Marker } from 'react-leaflet';
 import {Icon} from "leaflet/dist/leaflet-src.esm";
 
 export const Points = ({ points }) => (
   <>
     {points.map(({ Id: id, coordinates, Description: description, Image: image }) => (
       <>
-        <Marker position={coordinates} icon={new Icon({
+        <Marker key={id} position={coordinates} icon={new Icon({
           iconUrl: image,
           iconSize: [20, 30],
-        })}></Marker>
-      <Circle key={id} center={coordinates} className={"poi"}>
-        <Popup>
-          {description}
-        </Popup>
-      </Circle>
+        })}>
+          <Popup>
+            {description}
+          </Popup>
+        </Marker>
         </>
     ))}
   </>
