f53a432842fd4a1806f18bd06d5a7427b8fbe886
antontrofymchuk <antontrofymchuk@gmail.com>
2020-03-02 20:50:23 +0200

    fix for missing values on the pie charts

--- client/src/components/chart.jsx
+++ client/src/components/chart.jsx
@@ -4,22 +4,27 @@ import {
 } from 'recharts';
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const valuePrecision = 4;
 
-export const Chart = ({ data }) => (
+export const Chart = ({ data }) => {
+  const dataForView = data.map(item => ({...item, value: +item.value.toFixed(valuePrecision)}));
+  return (
     <PieChart width={400} height={400}>
-        <Pie
-            data={data}
-            cx={200}
-            cy={200}
-            label
-            labelLine={false}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-        >
-            {
-                data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-            }
-        </Pie>
+      <Pie
+        data={dataForView}
+        cx={200}
+        cy={200}
+        label
+        labelLine={false}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="value"
+        isAnimationActive={false}
+      >
+        {
+          data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>)
+        }
+      </Pie>
     </PieChart>
-);
+  );
+};
