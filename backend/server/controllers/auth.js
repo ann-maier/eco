36f28bba67cb04242fc63953ed4250dd0eefed9b
antontrofymchuk <antontrofymchuk@gmail.com>
2020-04-25 22:33:49 +0300

    send user name on login

--- backend/server/controllers/auth.js
+++ backend/server/controllers/auth.js
@@ -4,34 +4,22 @@ const login = (req, res) => {
   const { login, password } = req.body;
 
   const query = `
-  SELECT 
-   ??,
-    (SELECT ?? FROM ?? WHERE ?? = ? AND ?? = ?) as id_of_user
-  FROM 
-    ??
-  WHERE ?? =
-  (SELECT ?? FROM ?? WHERE ?? = ? AND ?? = ?);
-  `;
+    SELECT
+      user.user_name,
+      user.id_of_expert,
+      user.id_of_user,
+      expert.expert_name,
+      expert.expert_FIO
+    FROM
+      user
+    INNER JOIN
+      expert
+    ON 
+      user.id_of_expert = expert.id_of_expert
+    WHERE user.user_name = ${login} AND user.password = ${password}
+  ;`;
 
-  const values = [
-    ['expert_name', 'id_of_expert', 'expert_FIO'],
-    'id_of_user',
-    'user',
-    'user_name',
-    login,
-    'password',
-    password,
-    'expert',
-    'id_of_expert',
-    'id_of_expert',
-    'user',
-    'user_name',
-    login,
-    'password',
-    password,
-  ];
-
-  return pool.query(query, values, (error, rows) => {
+  return pool.query(query, [], (error, rows) => {
     if (error) {
       return res.status(500).send({
         message: error,
@@ -43,6 +31,7 @@ const login = (req, res) => {
       id_of_expert: rows[0].id_of_expert,
       FIO: rows[0].expert_FIO,
       id_of_user: rows[0].id_of_user,
+      user_name: rows[0].user_name,
     };
 
     return res.send(JSON.stringify(response));
